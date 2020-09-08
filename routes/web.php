5875e0b5bdf9332531019532a35e06689d2405fc
Zainab Awad <zainab.zainab@c2native.io>
2020-09-08 14:44:55 +0400

    auth route

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => 'auth.basic'], function () {
+    // User needs to be authenticated to enter here.
+    Route::get('/', function ()    {
+        return view('welcome');
+    });  
+});
